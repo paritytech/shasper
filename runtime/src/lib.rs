59b7c1baa00cb207dd1a16c5b856ea11b07907c7
Wei Tang <hi@that.world>
2018-09-19 23:48:31 +0800

    Implement shuffle

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -1,6 +1,7 @@
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
+extern crate blake2_rfc as blake2;
 extern crate parity_codec as codec;
 #[macro_use]
 extern crate parity_codec_derive;
--- runtime/src/validators.rs
+++ runtime/src/validators.rs
@@ -1,4 +1,5 @@
 use primitives::H256;
+use blake2::blake2s::blake2s;
 
 use super::Address;
 
@@ -13,6 +14,7 @@ pub struct ValidatorRecord {
 	pub end_dynasty: u64,
 }
 
+#[derive(Clone)]
 pub struct Validators(Vec<ValidatorRecord>);
 
 impl Validators {
@@ -21,5 +23,32 @@ impl Validators {
 			.filter(|v| v.start_dynasty <= dynasty && v.end_dynasty > dynasty)
 			.cloned()
 			.collect()
+	}
+
+	pub fn shuffle(&self, seed: H256) -> Vec<ValidatorRecord> {
+		let mut ret = self.0.clone();
+		assert!(ret.len() <= 16777216);
+		let mut source = seed;
+		let mut i = 0;
+
+		while i < ret.len() {
+			source = H256::from(blake2s(32, &[], &source).as_bytes());
+			for j in 0..10 {
+				let pos = j * 3;
+				let m = u32::from_be(unsafe { ::std::mem::transmute([0u8, source[pos], source[pos+1], source[pos+2]]) });
+				let remaining = ret.len() - i;
+				if remaining == 0 {
+					break;
+				}
+				let rand_max = 16777216 - 16777216 % remaining as u32;
+				if m < rand_max {
+					let replacement_pos = (m as usize % remaining) + i;
+					ret.swap(i, replacement_pos);
+					i += 1;
+				}
+			}
+		}
+
+		ret
 	}
 }
