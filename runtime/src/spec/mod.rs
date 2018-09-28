eacc913756f2d9274ea36fe78d6139698c6494ad
Wei Tang <hi@that.world>
2018-09-28 16:40:12 +0800

    Make all spec field public

--- runtime/src/spec/mod.rs
+++ runtime/src/spec/mod.rs
@@ -10,13 +10,13 @@ use attestation::AttestationRecord;
 #[cfg_attr(feature = "std", derive(Debug))]
 #[ssz_codec(sorted)]
 pub struct SpecHeader {
-	parent_hash: H256,
-	slot_number: u64,
-	randao_reveal: H256,
-	attestations: Vec<SpecAttestationRecord>,
-	pow_chain_ref: H256,
-	active_state_root: H256,
-	crystallized_state_root: H256,
+	pub parent_hash: H256,
+	pub slot_number: u64,
+	pub randao_reveal: H256,
+	pub attestations: Vec<SpecAttestationRecord>,
+	pub pow_chain_ref: H256,
+	pub active_state_root: H256,
+	pub crystallized_state_root: H256,
 }
 
 impl SpecHeader {
