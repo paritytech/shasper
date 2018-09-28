86261775ee5c028591659c73c5d067e1b2804bfa
Wei Tang <hi@that.world>
2018-09-28 15:53:32 +0800

    Use customized Default for attestation
    Needed because of aggregate_sig

--- runtime/src/attestation.rs
+++ runtime/src/attestation.rs
@@ -1,7 +1,7 @@
 use primitives::{H256, U256};
 use rstd::prelude::*;
 
-#[derive(Clone, PartialEq, Eq, Decode, Encode, Default, SszEncode, SszDecode)]
+#[derive(Clone, PartialEq, Eq, Decode, Encode, SszEncode, SszDecode)]
 #[cfg_attr(feature = "std", derive(Debug))]
 pub struct AttestationRecord {
 	pub slot: u64,
@@ -12,4 +12,19 @@ pub struct AttestationRecord {
 	pub justified_slot: u64,
 	pub justified_block_hash: H256,
 	pub aggregate_sig: Vec<U256>,
+}
+
+impl Default for AttestationRecord {
+	fn default() -> Self {
+		Self {
+			slot: 0,
+			shard_id: 0,
+			oplique_parent_hashes: vec![],
+			shard_block_hash: H256::new(),
+			attester_bitfield: vec![],
+			justified_slot: 0,
+			justified_block_hash: H256::new(),
+			aggregate_sig: vec![U256::zero(), U256::zero()],
+		}
+	}
 }
