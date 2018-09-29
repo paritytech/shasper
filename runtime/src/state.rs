395c1f694e669bcf7a0cf712aef1144e38dfff99
Wei Tang <hi@that.world>
2018-09-29 15:28:25 +0800

    shards_and_committees_for_slot

--- runtime/src/state.rs
+++ runtime/src/state.rs
@@ -1,6 +1,7 @@
 use primitives::H256;
 use rstd::prelude::*;
 
+use consts::CYCLE_LENGTH;
 use attestation::AttestationRecord;
 use validators::{Validators, ShardAndCommittee};
 
@@ -24,7 +25,7 @@ pub struct ActiveState {
 pub struct CrystallizedState {
 	pub validators: Validators,
 	pub last_state_recalc: u64,
-	pub shard_and_committee_for_slots: Vec<Vec<ShardAndCommittee>>,
+	pub shards_and_committees_for_slots: Vec<Vec<ShardAndCommittee>>,
 	pub last_justified_slot: u64,
 	pub justified_streak: u64,
 	pub last_finalized_slot: u64,
@@ -32,4 +33,13 @@ pub struct CrystallizedState {
 	pub crosslink_records: Vec<CrosslinkRecord>,
 	pub dynasty_seed: H256,
 	pub dynasty_start: u64,
+}
+
+impl CrystallizedState {
+	pub fn shards_and_committees_for_slot(&self, slot: u64) -> &[ShardAndCommittee] {
+		let slot = slot as usize;
+		let start = (self.last_state_recalc as usize).saturating_sub(CYCLE_LENGTH);
+		assert!(start <= slot, slot > start + CYCLE_LENGTH * 2);
+		&self.shards_and_committees_for_slots[slot - start]
+	}
 }
