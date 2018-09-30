18601bc6cc45d52ecebc2e0797528b877ff73bd3
Wei Tang <hi@that.world>
2018-09-30 17:48:35 +0800

    Fix compile

--- service/src/lib.rs
+++ service/src/lib.rs
@@ -1,9 +1,12 @@
 extern crate shasper_runtime as runtime;
+extern crate substrate_primitives as primitives;
+extern crate substrate_client as client;
 extern crate substrate_service as service;
 
 use runtime::Block;
+use primitives::Blake2Hasher;
 
-pub trait Components: serivce::Components {
+pub trait Components: service::Components {
 	type Backend: 'static + client::backend::Backend<Block, Blake2Hasher>;
 	type Executor: 'static + client::CallExecutor<Block, Blake2Hasher> + Send + Sync;
 }
