07a70c791f1b0be921f23ee4bda78fca7786a8c9
Wei Tang <hi@that.world>
2018-10-01 18:15:28 +0800

    Avoid committing wasm binary files

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -2,8 +2,11 @@
 
 set -eux
 
+sudo apt-get -y update
+sudo apt-get install -y cmake pkg-config libssl-dev
+
 # Install rustup and the specified rust toolchain.
-curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain=$RUST_TOOLCHAIN -y
+curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain=stable -y
 
 # Load cargo environment. Specifically, put cargo into PATH.
 source ~/.cargo/env
@@ -12,17 +15,7 @@ rustc --version
 rustup --version
 cargo --version
 
-case $TARGET in
-	"native")
-		sudo apt-get -y update
-		sudo apt-get install -y cmake pkg-config libssl-dev
+./scripts/init.sh
+./scripts/build.sh
 
-		cargo test --all --locked
-		;;
-
-	"wasm")
-		# Install prerequisites and build all wasm projects
-		./scripts/init.sh
-		./scripts/build.sh
-		;;
-esac
+cargo test --all --locked
