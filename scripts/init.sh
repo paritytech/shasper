e7d33071a5cb7c30d5a4ee2de84311dfb50792bd
David Dorgan <david@parity.io>
2018-10-05 01:24:08 +0100

    Do not rustup in a container to avoid crosslink errors

--- scripts/init.sh
+++ scripts/init.sh
@@ -4,14 +4,15 @@ set -e
 
 echo "*** Initialising WASM build environment"
 
-rustup update nightly
+if [ -z $CI_PROJECT_NAME ] ; then
+   rustup update nightly
+   rustup update stable
+fi
+
 rustup target add wasm32-unknown-unknown --toolchain nightly
-# Temporarily install llvm-tools.
-# See https://github.com/rust-lang/rust/issues/53813
-# TODO: Remove this when fixed.
+
 rustup component add llvm-tools-preview --toolchain nightly
-rustup update stable
 
 # Install wasm-gc. It's useful for stripping slimming down wasm binaries.
 command -v wasm-gc || \
-	cargo +nightly install --git https://github.com/alexcrichton/wasm-gc
+        cargo +nightly install --git https://github.com/alexcrichton/wasm-gc --force
