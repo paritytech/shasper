0410dd109e502659bff9b8f436564ed5bbcad8b7
Wei Tang <hi@that.world>
2018-12-10 19:15:44 +0800

    Implement justified head

--- lmd-ghost/src/lib.rs
+++ lmd-ghost/src/lib.rs
@@ -1,7 +1,49 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+extern crate shasper_primitives;
+extern crate sr_std as rstd;
+
+use rstd::collections::btree_map::BTreeMap;
+use shasper_primitives::{H384, H256, Count, Slot};
+
+pub struct SimplifiedTreeRoute {
+	pub retracted: Vec<(Slot, H256)>,
+	pub common: (Slot, H256),
+	pub enacted: Vec<(Slot, H256)>,
+}
+
+pub struct GlobalMetadata {
+	pub latest_attestation_targets: BTreeMap<H384, (Slot, H256)>,
+	pub finalized: (Slot, H256),
+	pub justified: Vec<(Slot, H256)>,
+}
+
+impl GlobalMetadata {
+	pub fn finalize(&mut self, block: (Slot, H256), retracted: Vec<(Slot, H256)>) {
+		self.justified.retain(|b| b.0 > block.0 && !retracted.iter().any(|c| c.1 == b.1));
+		self.finalized = block;
+	}
+
+	pub fn justify(&mut self, block: (Slot, H256)) {
+		self.justified.push(block);
+		self.justified.sort_by_key(|b| b.0);
+	}
+
+	pub fn justified_head(&self, current_slot: Slot, epoch_length: Slot) -> (Slot, H256) {
+		self.justified.last()
+			.and_then(|last| {
+				if current_slot >= last.0 + epoch_length {
+					Some(last.clone())
+				} else {
+					None
+				}
+			})
+			.unwrap_or(self.finalized.clone())
+	}
+}
+
+pub type BlockMetadata = Count;
+
+pub struct ImportRequest {
+	pub new_attestation_targets: BTreeMap<H384, (Slot, H256)>,
+	pub finalize: Option<((Slot, H256), Vec<(Slot, H256)>)>,
+	pub justify: Option<(Slot, H256)>,
 }
--- primitives/src/lib.rs
+++ primitives/src/lib.rs
@@ -1,3 +1,4 @@
+extern crate substrate_primitives;
 #[macro_use]
 extern crate fixed_hash;
 
@@ -5,3 +6,8 @@ construct_fixed_hash! {
 	/// Fixed 384-bit hash.
 	pub struct H384(48);
 }
+
+pub use substrate_primitives::H256;
+
+pub type Count = u64;
+pub type Slot = u64;
