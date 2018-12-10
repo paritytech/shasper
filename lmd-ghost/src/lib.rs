e54907f93aa381ae7f89616fecf0574ce7b65a39
Wei Tang <hi@that.world>
2018-12-10 20:22:08 +0800

    Add docs for lmd-ghost

--- lmd-ghost/src/lib.rs
+++ lmd-ghost/src/lib.rs
@@ -1,32 +1,59 @@
+// Copyright 2018 Parity Technologies (UK) Ltd.
+// This file is part of Substrate Shasper.
+
+// Substrate is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Substrate is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Substrate.  If not, see <http://www.gnu.org/licenses/>.
+
+//! LMD-GHOST fork choice rule implementation. Caller needs to first create a
+//! `FinalizationState`. When processing blocks, create an `ImportRequest` and
+//! pass it to `process`, and apply `ImportResult` to blockchain.
+
 extern crate shasper_primitives;
 extern crate sr_std as rstd;
 
 use rstd::collections::btree_map::BTreeMap;
 use shasper_primitives::{H256, Count, Slot};
 
+/// Simplified version of Substrate tree route.
 pub struct SimplifiedTreeRoute {
+	/// Retracted block slot, block hash, and current count metadata.
 	pub retracted: Vec<(Slot, H256, Count)>,
+	/// Common block slot, block hash, and current count metadata.
 	pub common: (Slot, H256, Count),
+	/// Enacted block slot, block hash, and current count metadata.
 	pub enacted: Vec<(Slot, H256, Count)>,
 }
 
+/// State related to finalization.
 pub struct FinalizationState {
+	/// Last finalized block slot and block hash.
 	pub finalized: (Slot, H256),
+	/// Justified block slots and block hashes.
 	pub justified: Vec<(Slot, H256)>,
 }
 
 impl FinalizationState {
-	pub fn finalize(&mut self, block: (Slot, H256), retracted: Vec<(Slot, H256)>) {
+	fn finalize(&mut self, block: (Slot, H256), retracted: Vec<(Slot, H256)>) {
 		self.justified.retain(|b| b.0 > block.0 && !retracted.iter().any(|c| c.1 == b.1));
 		self.finalized = block;
 	}
 
-	pub fn justify(&mut self, block: (Slot, H256)) {
+	fn justify(&mut self, block: (Slot, H256)) {
 		self.justified.push(block);
 		self.justified.sort_by_key(|b| b.0);
 	}
 
-	pub fn justified_head(&self, current_slot: Slot, epoch_length: Slot) -> (Slot, H256) {
+	fn justified_head(&self, current_slot: Slot, epoch_length: Slot) -> (Slot, H256) {
 		self.justified.last()
 			.and_then(|last| {
 				if current_slot >= last.0 + epoch_length {
@@ -81,19 +108,31 @@ impl<'a> VotedTreeRoute<'a> {
 	}
 }
 
+/// An import request.
 pub struct ImportRequest {
-	pub new_attestation_targets: Vec<SimplifiedTreeRoute>, // Tree route from old attestation target to new attestation target.
+	/// New attestation targets. The caller should keep track of the current validators'
+	/// attestation targets. When a new block is imported, a tree route should be calculated
+	/// from the current attestation target to new attestation target.
+	pub new_attestation_targets: Vec<SimplifiedTreeRoute>,
+	/// Finalizing block, if any, and retracted blocks due to finalization.
 	pub finalize: Option<((Slot, H256), Vec<(Slot, H256)>)>,
+	/// Justifying block, if any.
 	pub justify: Option<(Slot, H256)>,
+	/// Tree route from current head to new block.
 	pub route: SimplifiedTreeRoute,
+	/// New block's slot.
 	pub current_slot: Slot,
 }
 
+/// An import response. The caller should apply this to blockchain database.
 pub struct ImportResponse {
+	/// All updated vote counts to be applied to block metadata.
 	pub updated_vote_counts: BTreeMap<H256, Count>,
+	/// Whether the new block is the best block.
 	pub is_new_best: bool,
 }
 
+/// Process a new block given state and import request.
 pub fn process(state: &mut FinalizationState, request: ImportRequest, epoch_length: Slot) -> ImportResponse {
 	for finalize in request.finalize {
 		state.finalize(finalize.0, finalize.1);
