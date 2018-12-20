b4839bdf2680512c87faa59bc9d576f16aa6ae7c
Wei Tang <hi@that.world>
2018-12-20 19:59:11 +0800

    Add more type alias

--- crypto/src/lib.rs
+++ crypto/src/lib.rs
@@ -4,4 +4,6 @@ pub extern crate pairing;
 use pairing::bls12_381::Bls12;
 
 pub type BlsPair = bls::Keypair<Bls12>;
+pub type BlsPublicKey = bls::PublicKey<Bls12>;
+pub type BlsSignature = bls::Signature<Bls12>;
 pub type BlsAggregateSignature = bls::AggregateSignature<Bls12>;
