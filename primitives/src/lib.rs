b02926b98f36e05988f4e45e0169f36e7512dba6
Wei Tang <accounts@that.world>
2019-01-07 14:58:16 +0100

    Re-introduce aggregate signature support (#41)
    * Update bls version
    
    * Re-introduce aggregate signature support

--- primitives/src/lib.rs
+++ primitives/src/lib.rs
@@ -44,10 +44,12 @@ pub extern crate shasper_crypto as crypto;
 mod authority_id;
 mod bitfield;
 mod attestation;
+mod signature;
 
 use runtime_primitives::generic;
 use runtime_primitives::traits::{BlakeTwo256, Extrinsic as ExtrinsicT};
 
+pub use signature::{H768, Signature};
 pub use authority_id::{H384, AuthorityId};
 pub use bitfield::BitField;
 pub use attestation::AttestationRecord;
---
+++ primitives/src/signature.rs
@@ -0,0 +1,111 @@
+// Copyright 2017-2018 Parity Technologies (UK) Ltd.
+// This file is part of Substrate.
+
+// Substrate is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Substrate is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Substrate.  If not, see <http://www.gnu.org/licenses/>.
+
+#[cfg(feature = "std")]
+use serde::{Serialize, Serializer, Deserialize, Deserializer};
+use crypto::bls;
+use rstd::vec::Vec;
+
+#[cfg(feature = "std")]
+use primitives::bytes;
+
+const SIZE: usize = 96;
+
+construct_fixed_hash! {
+	/// Fixed 384-bit hash.
+	pub struct H768(SIZE);
+}
+
+pub type Signature = H768;
+
+#[cfg(feature = "std")]
+impl Serialize for H768 {
+	fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> where S: Serializer {
+		bytes::serialize(&self.0, serializer)
+	}
+}
+
+#[cfg(feature = "std")]
+impl<'de> Deserialize<'de> for H768 {
+	fn deserialize<D>(deserializer: D) -> Result<Self, D::Error> where D: Deserializer<'de> {
+		bytes::deserialize_check_len(deserializer, bytes::ExpectedLen::Exact(SIZE))
+			.map(|x| H768::from_slice(&x))
+	}
+}
+
+impl ::parity_codec::Encode for H768 {
+	fn using_encoded<R, F: FnOnce(&[u8]) -> R>(&self, f: F) -> R {
+		self.0.using_encoded(f)
+	}
+}
+impl ::parity_codec::Decode for H768 {
+	fn decode<I: ::parity_codec::Input>(input: &mut I) -> Option<Self> {
+		<[u8; SIZE] as ::parity_codec::Decode>::decode(input).map(H768)
+	}
+}
+
+impl ::ssz::Encode for H768 {
+	fn encode_to<W: ::ssz::Output>(&self, dest: &mut W) {
+		dest.write(self.as_ref())
+	}
+}
+impl ::ssz::Decode for H768 {
+	fn decode<I: ::ssz::Input>(input: &mut I) -> Option<Self> {
+		let mut vec = [0u8; SIZE];
+		if input.read(&mut vec[..SIZE]) != SIZE {
+			None
+		} else {
+			Some(H768::from(&vec))
+		}
+	}
+}
+
+impl ::ssz_hash::SpecHash for H768 {
+	fn spec_hash<H: ::hash_db::Hasher>(&self) -> H::Out {
+		let encoded = ssz::Encode::encode(self);
+		H::hash(&encoded)
+	}
+}
+
+impl H768 {
+	pub fn into_signature(&self) -> Option<bls::Signature> {
+		bls::Signature::from_compressed_bytes(self.as_ref())
+	}
+
+	pub fn into_aggregate_signature(&self) -> Option<bls::AggregateSignature> {
+		bls::AggregateSignature::from_compressed_bytes(self.as_ref())
+	}
+
+	pub fn from_signature(sig: bls::Signature) -> Self {
+		H768::from_slice(&sig.to_compressed_bytes())
+	}
+
+	pub fn from_aggregate_signature(sig: bls::AggregateSignature) -> Self {
+		H768::from_slice(&sig.to_compressed_bytes())
+	}
+}
+
+impl Into<Signature> for bls::Signature {
+	fn into(self) -> Signature {
+		Signature::from_signature(self)
+	}
+}
+
+impl Into<Signature> for bls::AggregateSignature {
+	fn into(self) -> Signature {
+		Signature::from_aggregate_signature(self)
+	}
+}
