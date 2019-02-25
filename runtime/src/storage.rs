59c0309907709e9106c2258714d5f4cd22b75640
Wei Tang <hi@that.world>
2019-02-25 15:58:39 +0100

    Insert genesis timestamp

--- runtime/src/storage.rs
+++ runtime/src/storage.rs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Substrate.  If not, see <http://www.gnu.org/licenses/>.
 
-use primitives::{BlockNumber, Hash, Epoch, Balance, ValidatorId, CheckedAttestation};
+use primitives::{BlockNumber, Hash, Epoch, Balance, Slot, ValidatorId, CheckedAttestation};
 use runtime_support::storage_items;
 use runtime_support::storage::StorageValue;
 use runtime_support::storage::unhashed::{self, StorageVec};
@@ -26,6 +26,7 @@ storage_items! {
 	pub ParentHash: b"sys:parenthash" => default Hash;
 	pub Digest: b"sys:digest" => default DigestT;
 	pub CasperContext: b"sys:caspercontext" => default casper::CasperContext<Epoch>;
+	pub GenesisSlot: b"sys:genesisslot" => default Slot;
 }
 
 pub struct UncheckedExtrinsics;
