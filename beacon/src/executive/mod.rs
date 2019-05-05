01fb254c362f50ace4af66a2681ebe86f5a7337d
Wei Tang <hi@that.world>
2019-05-06 01:09:39 +0200

    Add function to execute block

--- beacon/src/executive/mod.rs
+++ beacon/src/executive/mod.rs
@@ -20,8 +20,9 @@ mod genesis;
 
 pub use self::genesis::*;
 
-use crate::types::BeaconState;
-use crate::Config;
+use core::cmp::min;
+use crate::types::{BeaconState, BeaconBlock};
+use crate::{Config, Error};
 
 /// Beacon state executive.
 pub struct Executive<'state, 'config, C: Config> {
@@ -29,4 +30,77 @@ pub struct Executive<'state, 'config, C: Config> {
 	pub state: &'state mut BeaconState,
 	/// Beacon config.
 	pub config: &'config C,
+}
+
+/// Given a block, execute based on a parent state.
+pub fn execute_block<C: Config>(block: &BeaconBlock, state: &mut BeaconState, config: &C) -> Result<(), Error> {
+	let mut executive = Executive {
+		state, config
+	};
+
+	while executive.state.slot < block.slot {
+		executive.cache_state();
+
+		if (executive.state.slot + 1) % config.slots_per_epoch() == 0 {
+			executive.process_justification_and_finalization()?;
+			executive.process_crosslinks()?;
+			executive.process_rewards_and_penalties()?;
+			executive.process_registry_updates()?;
+			executive.process_slashings();
+			executive.process_final_updates();
+		}
+
+		executive.advance_slot();
+	}
+
+	executive.process_block_header(block)?;
+	executive.process_randao(block)?;
+	executive.process_eth1_data(block);
+
+	if block.body.proposer_slashings.len() > config.max_proposer_slashings() as usize {
+		return Err(Error::TooManyProposerSlashings)
+	}
+	for slashing in &block.body.proposer_slashings {
+		executive.process_proposer_slashing(slashing.clone())?;
+	}
+
+	if block.body.attester_slashings.len() > config.max_attester_slashings() as usize {
+		return Err(Error::TooManyAttesterSlashings)
+	}
+	for slashing in &block.body.attester_slashings {
+		executive.process_attester_slashing(slashing.clone())?;
+	}
+
+	if block.body.attestations.len() > config.max_attestations() as usize {
+		return Err(Error::TooManyAttestations)
+	}
+	for attestation in &block.body.attestations {
+		executive.process_attestation(attestation.clone())?;
+	}
+
+	if block.body.deposits.len() != min(
+		config.max_deposits(),
+		executive.state.latest_eth1_data.deposit_count - executive.state.deposit_index
+	) as usize {
+		return Err(Error::TooManyDeposits)
+	}
+	for deposit in &block.body.deposits {
+		executive.process_deposit(deposit.clone())?;
+	}
+
+	if block.body.voluntary_exits.len() > config.max_voluntary_exits() as usize {
+		return Err(Error::TooManyVoluntaryExits)
+	}
+	for voluntary_exit in &block.body.voluntary_exits {
+		executive.process_voluntary_exit(voluntary_exit.clone())?;
+	}
+
+	if block.body.transfers.len() > config.max_transfers() as usize{
+		return Err(Error::TooManyTransfers)
+	}
+	for transfer in &block.body.transfers {
+		executive.process_transfer(transfer.clone())?;
+	}
+
+	Ok(())
 }
