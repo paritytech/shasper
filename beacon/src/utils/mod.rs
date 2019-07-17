110131229610ea8e43984b99088781dd93068901
Wei Tang <hi@that.world>
2019-07-17 19:43:08 +0200

    beacon: port back math::integer_squareroot

--- beacon/src/utils/mod.rs
+++ beacon/src/utils/mod.rs
@@ -3,3 +3,13 @@ mod serde;
 
 #[cfg(feature = "serde")]
 pub use self::serde::*;
+
+pub fn integer_squareroot(n: u64) -> u64 {
+	let mut x = n;
+	let mut y = (x + 1) / 2;
+	while y < x {
+		x = y;
+		y = (x + n / x) / 2
+	}
+	x
+}
