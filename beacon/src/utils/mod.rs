cd6192945348b139a6587559bf619de0cc14a2f6
Wei Tang <hi@that.world>
2019-07-17 19:44:43 +0200

    beacon: port back to_bytes/to_uint

--- beacon/src/utils/mod.rs
+++ beacon/src/utils/mod.rs
@@ -4,6 +4,21 @@ mod serde;
 #[cfg(feature = "serde")]
 pub use self::serde::*;
 
+use primitive_types::H256;
+
+pub fn to_bytes(v: u64) -> H256 {
+	let bytes = v.to_le_bytes();
+	let mut ret = H256::default();
+	(&mut ret[0..bytes.len()]).copy_from_slice(&bytes);
+	ret
+}
+
+pub fn to_uint(v: &[u8]) -> u64 {
+	let mut ret = 0u64.to_le_bytes();
+	(&mut ret[..]).copy_from_slice(&v[..v.len()]);
+	u64::from_le_bytes(ret)
+}
+
 pub fn integer_squareroot(n: u64) -> u64 {
 	let mut x = n;
 	let mut y = (x + 1) / 2;
