2405c7c0e3ed5ba1374d719a61ceb9e6eb532b9a
Wei Tang <hi@that.world>
2019-07-24 17:18:17 +0200

    ssztests: embed all yamltests directly in rust tests (under spectests feature)

--- yamltests/ssztests/src/main.rs
+++ yamltests/ssztests/src/main.rs
@@ -136,3 +136,39 @@ fn main() {
 		_ => panic!("Unknown config"),
 	}
 }
+
+#[cfg(all(test, feature = "spectests"))]
+mod spectests {
+	use super::*;
+
+	macro_rules! run {
+		( $name:ident, $config:ty, $path:expr ) => {
+			#[test]
+			fn $name() {
+				let reader = BufReader::new(include_bytes!($path).as_ref());
+				let coll = serde_yaml::from_reader::<_, Collection<$config>>(reader)
+					.expect("parse test cases failed");
+				for test in coll.test_cases {
+					test.test()
+				}
+			}
+		}
+	}
+
+	run!(ssz_mainnet_random, MainnetConfig,
+		 "../../spectests/tests/ssz_static/core/ssz_mainnet_random.yaml");
+	run!(ssz_minimal_lengthy, MinimalConfig,
+		 "../../spectests/tests/ssz_static/core/ssz_minimal_lengthy.yaml");
+	run!(ssz_minimal_max, MinimalConfig,
+		 "../../spectests/tests/ssz_static/core/ssz_minimal_max.yaml");
+	run!(ssz_minimal_nil, MinimalConfig,
+		 "../../spectests/tests/ssz_static/core/ssz_minimal_nil.yaml");
+	run!(ssz_minimal_one, MinimalConfig,
+		 "../../spectests/tests/ssz_static/core/ssz_minimal_one.yaml");
+	run!(ssz_minimal_random_chaos, MinimalConfig,
+		 "../../spectests/tests/ssz_static/core/ssz_minimal_random_chaos.yaml");
+	run!(ssz_minimal_random, MinimalConfig,
+		 "../../spectests/tests/ssz_static/core/ssz_minimal_random.yaml");
+	run!(ssz_minimal_zero, MinimalConfig,
+		 "../../spectests/tests/ssz_static/core/ssz_minimal_zero.yaml");
+}
