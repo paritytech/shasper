44a2b7a3251747386c44dd61fa1ffd925ab37723
Wei Tang <hi@that.world>
2019-08-10 15:16:54 +0200

    Init template for srml-casper

---
+++ substrate/casper/template/src/chain_spec.rs
@@ -0,0 +1,112 @@
+use primitives::{sr25519, Pair};
+use runtime::{
+	AccountId, GenesisConfig, AuraConfig, BalancesConfig,
+	SudoConfig, IndicesConfig, SystemConfig, WASM_BINARY, AuraId
+};
+use aura_primitives::sr25519::AuthorityPair as AuraPair;
+use substrate_service;
+
+// Note this is the URL for the telemetry server
+//const STAGING_TELEMETRY_URL: &str = "wss://telemetry.polkadot.io/submit/";
+
+/// Specialized `ChainSpec`. This is a specialization of the general Substrate ChainSpec type.
+pub type ChainSpec = substrate_service::ChainSpec<GenesisConfig>;
+
+/// The chain specification option. This is expected to come in from the CLI and
+/// is little more than one of a number of alternatives which can easily be converted
+/// from a string (`--chain=...`) into a `ChainSpec`.
+#[derive(Clone, Debug)]
+pub enum Alternative {
+	/// Whatever the current runtime is, with just Alice as an auth.
+	Development,
+	/// Whatever the current runtime is, with simple Alice/Bob auths.
+	LocalTestnet,
+}
+
+fn authority_key(s: &str) -> AuraId {
+	AuraPair::from_string(&format!("//{}", s), None)
+		.expect("static values are valid; qed")
+		.public()
+}
+
+fn account_key(s: &str) -> AccountId {
+	sr25519::Pair::from_string(&format!("//{}", s), None)
+		.expect("static values are valid; qed")
+		.public()
+}
+
+impl Alternative {
+	/// Get an actual chain config from one of the alternatives.
+	pub(crate) fn load(self) -> Result<ChainSpec, String> {
+		Ok(match self {
+			Alternative::Development => ChainSpec::from_genesis(
+				"Development",
+				"dev",
+				|| testnet_genesis(vec![
+					authority_key("Alice")
+				], vec![
+					account_key("Alice")
+				],
+					account_key("Alice")
+				),
+				vec![],
+				None,
+				None,
+				None,
+				None
+			),
+			Alternative::LocalTestnet => ChainSpec::from_genesis(
+				"Local Testnet",
+				"local_testnet",
+				|| testnet_genesis(vec![
+					authority_key("Alice"),
+					authority_key("Bob"),
+				], vec![
+					account_key("Alice"),
+					account_key("Bob"),
+					account_key("Charlie"),
+					account_key("Dave"),
+					account_key("Eve"),
+					account_key("Ferdie"),
+				],
+					account_key("Alice"),
+				),
+				vec![],
+				None,
+				None,
+				None,
+				None
+			),
+		})
+	}
+
+	pub(crate) fn from(s: &str) -> Option<Self> {
+		match s {
+			"dev" => Some(Alternative::Development),
+			"" | "local" => Some(Alternative::LocalTestnet),
+			_ => None,
+		}
+	}
+}
+
+fn testnet_genesis(initial_authorities: Vec<AuraId>, endowed_accounts: Vec<AccountId>, root_key: AccountId) -> GenesisConfig {
+	GenesisConfig {
+		system: Some(SystemConfig {
+			code: WASM_BINARY.to_vec(),
+			changes_trie_config: Default::default(),
+		}),
+		aura: Some(AuraConfig {
+			authorities: initial_authorities.clone(),
+		}),
+		indices: Some(IndicesConfig {
+			ids: endowed_accounts.clone(),
+		}),
+		balances: Some(BalancesConfig {
+			balances: endowed_accounts.iter().cloned().map(|k|(k, 1 << 60)).collect(),
+			vesting: vec![],
+		}),
+		sudo: Some(SudoConfig {
+			key: root_key,
+		}),
+	}
+}
---
+++ substrate/casper/template/src/service.rs
@@ -0,0 +1,134 @@
+//! Service and ServiceFactory implementation. Specialized wrapper over Substrate service.
+
+#![warn(unused_extern_crates)]
+
+use std::sync::Arc;
+use transaction_pool::{self, txpool::{Pool as TransactionPool}};
+use runtime::{self, GenesisConfig, opaque::Block, RuntimeApi, WASM_BINARY};
+use substrate_service::{
+	FactoryFullConfiguration, LightComponents, FullComponents, FullBackend,
+	FullClient, LightClient, LightBackend, FullExecutor, LightExecutor,
+	error::{Error as ServiceError},
+};
+use basic_authorship::ProposerFactory;
+use consensus::{import_queue, start_aura, AuraImportQueue, SlotDuration};
+use futures::prelude::*;
+use substrate_client::{self as client, LongestChain};
+use inherents::InherentDataProviders;
+use network::{config::DummyFinalityProofRequestBuilder, construct_simple_protocol};
+use substrate_executor::native_executor_instance;
+use substrate_service::construct_service_factory;
+use aura_primitives::sr25519::AuthorityPair as AuraAuthorityPair;
+
+pub use substrate_executor::NativeExecutor;
+// Our native executor instance.
+native_executor_instance!(
+	pub Executor,
+	runtime::api::dispatch,
+	runtime::native_version,
+	WASM_BINARY
+);
+
+#[derive(Default)]
+pub struct NodeConfig {
+	inherent_data_providers: InherentDataProviders,
+}
+
+construct_simple_protocol! {
+	/// Demo protocol attachment for substrate.
+	pub struct NodeProtocol where Block = Block { }
+}
+
+construct_service_factory! {
+	struct Factory {
+		Block = Block,
+		RuntimeApi = RuntimeApi,
+		NetworkProtocol = NodeProtocol { |config| Ok(NodeProtocol::new()) },
+		RuntimeDispatch = Executor,
+		FullTransactionPoolApi = transaction_pool::ChainApi<
+			client::Client<FullBackend<Self>, FullExecutor<Self>, Block, RuntimeApi>,
+			Block
+		> {
+			|config, client| Ok(TransactionPool::new(config, transaction_pool::ChainApi::new(client)))
+		},
+		LightTransactionPoolApi = transaction_pool::ChainApi<
+			client::Client<LightBackend<Self>, LightExecutor<Self>, Block, RuntimeApi>,
+			Block
+		> {
+			|config, client| Ok(TransactionPool::new(config, transaction_pool::ChainApi::new(client)))
+		},
+		Genesis = GenesisConfig,
+		Configuration = NodeConfig,
+		FullService = FullComponents<Self>
+			{ |config: FactoryFullConfiguration<Self>|
+				FullComponents::<Factory>::new(config)
+			},
+		AuthoritySetup = {
+			|service: Self::FullService| {
+				if service.config().roles.is_authority() {
+					let proposer = ProposerFactory {
+						client: service.client(),
+						transaction_pool: service.transaction_pool(),
+					};
+					let client = service.client();
+					let select_chain = service.select_chain()
+						.ok_or_else(|| ServiceError::SelectChainRequired)?;
+					let aura = start_aura::<_, _, _, _, _, AuraAuthorityPair, _, _, _>(
+						SlotDuration::get_or_compute(&*client)?,
+						client.clone(),
+						select_chain,
+						client,
+						proposer,
+						service.network(),
+						service.config().custom.inherent_data_providers.clone(),
+						service.config().force_authoring,
+						Some(service.keystore()),
+					)?;
+					service.spawn_task(Box::new(aura.select(service.on_exit()).then(|_| Ok(()))));
+				}
+
+				Ok(service)
+			}
+		},
+		LightService = LightComponents<Self>
+			{ |config| <LightComponents<Factory>>::new(config) },
+		FullImportQueue = AuraImportQueue<
+			Self::Block,
+		>
+			{ |config: &mut FactoryFullConfiguration<Self> , client: Arc<FullClient<Self>>, _select_chain: Self::SelectChain| {
+					import_queue::<_, _, aura_primitives::sr25519::AuthorityPair>(
+						SlotDuration::get_or_compute(&*client)?,
+						Box::new(client.clone()),
+						None,
+						None,
+						client,
+						config.custom.inherent_data_providers.clone(),
+					).map_err(Into::into)
+				}
+			},
+		LightImportQueue = AuraImportQueue<
+			Self::Block,
+		>
+			{ |config: &mut FactoryFullConfiguration<Self>, client: Arc<LightClient<Self>>| {
+					let fprb = Box::new(DummyFinalityProofRequestBuilder::default()) as Box<_>;
+					import_queue::<_, _, AuraAuthorityPair>(
+						SlotDuration::get_or_compute(&*client)?,
+						Box::new(client.clone()),
+						None,
+						None,
+						client,
+						config.custom.inherent_data_providers.clone(),
+					).map(|q| (q, fprb)).map_err(Into::into)
+				}
+			},
+		SelectChain = LongestChain<FullBackend<Self>, Self::Block>
+			{ |config: &FactoryFullConfiguration<Self>, client: Arc<FullClient<Self>>| {
+				#[allow(deprecated)]
+				Ok(LongestChain::new(client.backend().clone()))
+			}
+		},
+		FinalityProofProvider = { |_client: Arc<FullClient<Self>>| {
+			Ok(None)
+		}},
+	}
+}
