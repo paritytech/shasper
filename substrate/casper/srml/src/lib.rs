3d3b483bc938febd6ccf4e3a50a44c9527134ce6
Wei Tang <hi@that.world>
2019-08-10 14:42:44 +0200

    srml: OneSessionHandler

--- substrate/casper/srml/src/lib.rs
+++ substrate/casper/srml/src/lib.rs
@@ -78,4 +78,23 @@ decl_module! {
 // functions that do not write to storage and operation functions that do.
 // - Private functions. These are your usual private utilities unavailable to other modules.
 impl<T: Trait> Module<T> {
+
+}
+
+impl<T: Trait> session::OneSessionHandler<T::AccountId> for Module<T> {
+	type Key = ValidatorId;
+
+	fn on_new_session<'a, I: 'a>(changed: bool, validators: I, _queued_validators: I) where
+		I: Iterator<Item=(&'a T::AccountId, ValidatorId)>
+	{
+		if changed {
+			<Validators>::put(validators.map(|(_, k)| (k, 1u64)).collect::<Vec<_>>());
+		}
+	}
+
+	fn on_disabled(i: usize) {
+		let mut validators = <Validators>::get();
+		validators[i].1 = 0;
+		<Validators>::put(validators);
+	}
 }
