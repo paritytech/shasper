3bf739a2f7e520b77b469a490ed0c9e04ee2a37e
Wei Tang <hi@that.world>
2019-08-14 12:22:52 +0200

    Init beacon wasm

---
+++ beacon/wasm/index.js
@@ -0,0 +1,8 @@
+// Note that a dynamic `import` statement here is required due to
+// webpack/webpack#6615, but in theory `import { greet } from './pkg/hello_world';`
+// will work here one day as well!
+const rust = import('./pkg/beacon_wasm');
+
+rust
+  .then(m => m.greet('World!'))
+  .catch(console.error);
