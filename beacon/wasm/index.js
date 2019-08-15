48eba9d1f778d6227b2f3050b109dfd9efc7e351
Wei Tang <hi@that.world>
2019-08-15 16:11:34 +0200

    wasm: fix U1099511627776 exceeding u32 limit

--- beacon/wasm/index.js
+++ beacon/wasm/index.js
@@ -1,7 +1,7 @@
 // Note that a dynamic `import` statement here is required due to
 // webpack/webpack#6615, but in theory `import { greet } from './pkg/hello_world';`
 // will work here one day as well!
-const rust = import('./pkg/beacon_wasm');
+const rust = import('./pkg/eth2');
 
 const state = {
 	"genesis_time": 0,
