7d93955d9553f460154398f1c5eca55c4427207a
Wei Tang <hi@that.world>
2019-08-16 13:54:25 +0200

    beacon-wasm: use the builtin serde feature provided by wasm-bindgen

--- beacon/wasm/src/lib.rs
+++ beacon/wasm/src/lib.rs
@@ -1,7 +1,6 @@
 use wasm_bindgen::prelude::*;
 use beacon::{BLSNoVerification, BLSConfig, Config, MinimalConfig, MainnetConfig, BeaconState};
 use beacon::types::BeaconBlock;
-use js_sys::JSON;
 
 #[wasm_bindgen]
 pub fn execute_minimal(block: &JsValue, state: &JsValue) -> Result<JsValue, JsValue> {
@@ -16,16 +15,12 @@ pub fn execute_mainnet(block: &JsValue, state: &JsValue) -> Result<JsValue, JsVa
 fn execute<C: Config, BLS: BLSConfig>(block: &JsValue, state: &JsValue) -> Result<JsValue, JsValue> where
 	C: serde::Serialize + serde::de::DeserializeOwned,
 {
-	let block_string: String = JSON::stringify(block)?.into();
-	let block: BeaconBlock<C> = serde_json::from_str(&block_string)
-		.map_err(|e| JsValue::from(format!("{:?}", e)))?;
-	let state_string: String = JSON::stringify(state)?.into();
-	let mut state: BeaconState<C> = serde_json::from_str(&state_string)
-		.map_err(|e| JsValue::from(format!("{:?}", e)))?;
+	let block: BeaconBlock<C> = block.into_serde().map_err(|e| JsValue::from(format!("{:?}", e)))?;
+	let mut state: BeaconState<C> = state.into_serde().map_err(|e| JsValue::from(format!("{:?}", e)))?;
 
 	beacon::execute_block::<C, BLS>(
 		&block, &mut state
 	).map_err(|e| JsValue::from(format!("{:?}", e)))?;
 
-	Ok(JSON::parse(&serde_json::to_string(&state).map_err(|e| JsValue::from(format!("{:?}", e)))?)?)
+	Ok(JsValue::from_serde(&state).map_err(|e| JsValue::from(format!("{:?}", e)))?)
 }
