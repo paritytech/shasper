cf88b3e9d06a6c6749a386eb7bf4f138036fdbc4
Denis P <denis.pisarev@parity.io>
2019-10-29 15:30:02 +0100

    optimized init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,14 +5,10 @@ set -e
 echo "*** Initialising WASM build environment"
 
 if [ -z $CI_PROJECT_NAME ] ; then
-   rustup update nightly
-   rustup update stable
+      rustup update nightly
+      rustup update stable
+      rustup target add wasm32-unknown-unknown --toolchain nightly
+      # Install wasm-gc. It's useful for stripping slimming down wasm binaries.
+      command -v wasm-gc || \
+            cargo +nightly install --git https://github.com/alexcrichton/wasm-gc --force
 fi
-
-rustup target add wasm32-unknown-unknown --toolchain nightly
-
-rustup component add llvm-tools-preview --toolchain nightly
-
-# Install wasm-gc. It's useful for stripping slimming down wasm binaries.
-command -v wasm-gc || \
-      cargo +nightly install --git https://github.com/alexcrichton/wasm-gc --force
